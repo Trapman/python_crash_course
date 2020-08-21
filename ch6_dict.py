2138aa68d054bd1a60597ca748772b0745e11cb9
Trapman <david.a.shuttleworth@gmail.com>
2020-08-20 20:15:11 -0400

    Create ch6_dict.py

---
+++ ch6_dict.py
@@ -0,0 +1,13 @@
+# key-value pairs
+# dict = {}
+# dict = {'key':'value}
+
+sixers_roster = {'player': 'embiid', 'points':28}
+
+#assessing the vales in a dict
+print(sixers_roster['player'])
+print(sixers_roster['points'])
+
+total_points = sixers_roster['points']
+home_player = sixers_roster['player']
+print(f"The total points score for {home_player} was {total_points} for the game.")

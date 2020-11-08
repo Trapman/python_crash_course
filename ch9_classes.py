7be7ae020fa5c1ed0989d617d734717781150f37
Trapman <david.a.shuttleworth@gmail.com>
2020-11-07 21:14:14 -0500

    Update ch9_classes.py

--- ch9_classes.py
+++ ch9_classes.py
@@ -15,6 +15,29 @@ class Dog:
         """simulate a dog rolling over in response to a command"""
         print(f"{self.name} rolled over!")
 
-######
-############
-###
+#############
+# making an instance from a class
+# basically the class is a set of instructions for how to make an instance
+
+my_dog = Dog('Henley', 5)
+print(f"My dog's name is {my_dog.name}.")
+print(f"My dog is {my_dog.age} years old.")
+
+# accessing attributes
+my_dog.name
+my_dog.age
+
+# calling methods
+my_dog.sit()
+my_dog.roll_over()
+
+# creating multiple instances
+my_dog = Dog('Henley', 5)
+savuol_dog = Dog('Buford', 6)
+
+print(f"My dog's name is {my_dog.name}.")
+print(f"Savuol's dog's name is {savuol_dog.name}")
+
+print(f"n\My dog is {my_dog.age} years old.")
+your_dog.sit
+
